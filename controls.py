cf2e4590bb63b1c9a1d09b2da364721b6a4e8cbf
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2022-11-23 13:37:41 +0500

    enemy movements

--- controls.py
+++ controls.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from shot import Shot
+from enemy import Enemy
 
 def events(screen, ship, shots):
     """обработка событий"""
@@ -25,10 +26,39 @@ def events(screen, ship, shots):
             elif event.key == pygame.K_a:
                 ship.mleft = False
 
-def update(bg_color, screen, ship, shots):
+def update(bg_color, screen, ship, enemies, shots):
     """обновления экрана"""
     screen.fill(bg_color)
     for shot in shots.sprites():
         shot.draw_shot()
     ship.output()
+    enemies.draw(screen)
     pygame.display.flip()
+
+def update_shots(shots):
+    """обновления позиции пуль"""
+    shots.update()
+    for shot in shots.copy():
+        if shot.rect.bottom <= 0:
+            shots.remove(shot)
+
+def update_enemies(enemies):
+    enemies.update()
+
+def create_army(screen, enemies):
+    """создания армию"""
+    enemy = Enemy(screen)
+    enemy_width = enemy.rect.width
+    number_enemy_x = int((700 - 2 * enemy_width) / enemy_width)
+    enemy_height = enemy.rect.height
+    number_enemy_y = int((800 - 100 - 2) / enemy_height)
+
+    for row_number in range(number_enemy_y - 5):
+
+        for enemy_number in range(number_enemy_x):
+            enemy = Enemy(screen)
+            enemy.x = enemy_width + enemy_width * enemy_number
+            enemy.y = enemy_height + enemy_height * row_number
+            enemy.rect.x = enemy.x
+            enemy.rect.y = enemy.rect.height + enemy.rect.height * row_number
+            enemies.add(enemy)
