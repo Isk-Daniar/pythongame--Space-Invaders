46802d2cedfa2bbb485188fb51d7b381a209f6a7
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2022-11-28 12:02:23 +0500

    displaying score

--- controls.py
+++ controls.py
@@ -27,48 +27,61 @@ def events(screen, ship, shots):
             elif event.key == pygame.K_a:
                 ship.mleft = False
 
-def update(bg_color, screen, ship, enemies, shots):
+def update(bg_color, screen, stats, sc, ship, enemies, shots):
     """обновления экрана"""
     screen.fill(bg_color)
+    sc.show_score()
     for shot in shots.sprites():
         shot.draw_shot()
     ship.output()
     enemies.draw(screen)
     pygame.display.flip()
 
-def update_shots(screen, enemies, shots):
+def update_shots(screen, stats, sc, enemies, shots):
     """обновления позиции пуль"""
     shots.update()
     for shot in shots.copy():
         if shot.rect.bottom <= 0:
             shots.remove(shot)
     collisions = pygame.sprite.groupcollide(shots,enemies, True, True)
+    if collisions:
+        for enemies in collisions.values():
+            stats.score += 5 * len(enemies)
+        stats.score += 10
+        sc.image_score()
+        check_high_score(stats, sc)
+        sc.image_ships()
     if len(enemies) == 0:
         shots.empty()
         create_army(screen, enemies)
 
-def ship_kill (stats, screen, ship, enemies, shots):
+def ship_kill (stats, screen, sc, ship, enemies, shots):
     """столкновения корабля и армии"""
-    stats.ships_left -= 1
-    enemies.empty()
-    shots.empty()
-    create_army(screen, enemies)
-    ship.create_ship()
-    time.sleep(1)
+    if stats.ships_left > 0:
+        stats.ships_left -= 1
+        sc.image_ships()
+        enemies.empty()
+        shots.empty()
+        create_army(screen, enemies)
+        ship.create_ship()
+        time.sleep(1)
+    else:
+        stats.run_game = False
+        sys.exit()
 
-def update_enemies(stats, screen, ship, enemies, shots):
+def update_enemies(stats, screen, sc, ship, enemies, shots):
     """обновления позиции врагов"""
     enemies.update()
     if pygame.sprite.spritecollideany(ship, enemies):
-        ship_kill(stats, screen, ship, enemies, shots)
-    enemies_check(stats, screen, ship, enemies, shots)
+        ship_kill(stats, screen, sc, ship, enemies, shots)
+    enemies_check(stats, screen, sc, ship, enemies, shots)
 
-def enemies_check(stats, screen, ship, enemies, shots):
+def enemies_check(stats, screen, sc, ship, enemies, shots):
     """проверка, добралась ли армия до края экрна"""
     screen_rect = screen.get_rect()
     for enemy in enemies.sprites():
         if enemy.rect.bottom >= screen_rect.bottom:
-            ship_kill(stats, screen, ship, enemies, shots)
+            ship_kill(stats, screen, sc, ship, enemies, shots)
             break
 
 def create_army(screen, enemies):
@@ -77,7 +90,7 @@ def create_army(screen, enemies):
     enemy_width = enemy.rect.width
     number_enemy_x = int((700 - 2 * enemy_width) / enemy_width)
     enemy_height = enemy.rect.height
-    number_enemy_y = int((800 - 100 - 2) / enemy_height)
+    number_enemy_y = int((800 - 500 - 2) / enemy_height)
 
     for row_number in range(number_enemy_y - 5):
 
@@ -88,3 +101,11 @@ def create_army(screen, enemies):
             enemy.rect.x = enemy.x
             enemy.rect.y = enemy.rect.height + enemy.rect.height * row_number
             enemies.add(enemy)
+
+def check_high_score(stats, sc):
+    """проверка рекорда"""
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sc.image_high_score()
+        with open('highscore.txt', 'w') as f:
+            f.write(str(stats.high_score ))
--- stats.py
+++ stats.py
@@ -4,7 +4,11 @@ class Stats():
     def __init__(self):
         """иницивлизирует статистику"""
         self.reset_stats()
+        self.run_game = True
+        with open('highscore.txt', 'r') as f:
+            self.high_score = int(f.readline())
 
     def reset_stats(self):
         """статистика, изменяющаяся во время игры"""
         self.ships_left = 2
+        self.score = 0
